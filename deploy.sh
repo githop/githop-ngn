b85b88a68f037978120d96233b2980b945399a36
githop <tom@githop.com>
2017-03-23 22:52:43 -0600

    chore(deps): update to angular 4.0.0 @angular/cli 1.0.0

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+rm -rf dist/
 ng build --prod --aot
+find -E dist/ -regex "".*\js\$"" -exec bash -c 'echo Compressing "{}" && gzip -c --best "{}" > "{}.gz"' \;
 rm -rf ../../ruby/githop_api/public/*
 cp -r dist/ ../../ruby/githop_api/public/
 cp bust.manifest ../../ruby/githop_api/public/
