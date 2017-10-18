4e5daa3b664c0fafa25730945832122a11a113ca
githop <tom@githop.com>
2017-10-18 00:26:51 -0600

    updates(service-worker): update ngsw-manifest.json

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf dist/
-ng build --prod --aot
+ng build --prod --aot --base-href="https://githop.com/"
 find -E dist/ -regex "".*\js\$"" -exec bash -c 'echo Compressing "{}" && gzip -c --best "{}" > "{}.gz"' \;
 rm -rf ../../ruby/githop_api/public/*
 cp -r dist/ ../../ruby/githop_api/public/
