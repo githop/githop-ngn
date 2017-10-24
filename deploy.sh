cf3ae6a7a6878db732ec83aceea05e246e3be495
githop <tom@githop.com>
2017-10-23 20:22:27 -0600

    updates(deploy script): remove ref to deprecated file

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,3 @@ cp pwa-manifest.json dist/
 find -E dist/ -regex "".*\js\$"" -exec bash -c 'echo Compressing "{}" && gzip -c --best "{}" > "{}.gz"' \;
 rm -rf ../../ruby/githop_api/public/*
 cp -r dist/ ../../ruby/githop_api/public/
-cp bust.manifest ../../ruby/githop_api/public/
