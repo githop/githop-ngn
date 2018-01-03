e8040fb52adc0b77ff81104dfc27a36066f9bacb
githop <tom@githop.com>
2018-01-02 20:16:42 -0700

    chore(updates): tweak build script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 rm -rf dist/
-ng build --prod --aot --base-href="https://githop.com/"
-cp pwa-manifest.json dist/
+ng build --prod
 find -E dist/ -regex "".*\js\$"" -exec bash -c 'echo Compressing "{}" && gzip -c --best "{}" > "{}.gz"' \;
 rm -rf ../../ruby/githop_api/public/*
 cp -r dist/ ../../ruby/githop_api/public/
