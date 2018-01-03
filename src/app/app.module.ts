0615a393d069a97cf0956e45201e569294462037
githop <tom@githop.com>
2018-01-02 19:55:08 -0700

    chore(updates): latest angular, cli, update service worker config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { LandingModule } from "./landing/landing.module";
+import { LandingModule } from './landing/landing.module';
 
 import { AppComponent } from './app.component';
-import {AppRoutesModule} from "./app.routes";
-import { BlogModule } from "./blog/blog.module";
-import {HttpClientModule} from '@angular/common/http';
+import { AppRoutesModule } from './app.routes';
+import { BlogModule } from './blog/blog.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,11 @@ import {HttpClientModule} from '@angular/common/http';
     HttpClientModule,
     LandingModule,
     AppRoutesModule,
-    BlogModule.forRoot()
+    BlogModule.forRoot(),
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
